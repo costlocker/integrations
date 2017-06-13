ee1d7595140ae25066a86087c83ad94f3c2b3e79
Zdeněk Drahoš <drahoszdenek@gmail.com>
2017-06-13 10:27:26 +0200

    Basecamp - load default basecamp user from current company

--- basecamp/backend/src/Database/UpdateSettings.php
+++ basecamp/backend/src/Database/UpdateSettings.php
@@ -5,7 +5,6 @@ namespace Costlocker\Integrations\Database;
 use Doctrine\ORM\EntityManagerInterface;
 use Costlocker\Integrations\Auth\GetUser;
 use Costlocker\Integrations\Costlocker\RegisterWebhook;
-use Costlocker\Integrations\Entities\BasecampUser;
 use Costlocker\Integrations\Entities\CostlockerUser;
 use Costlocker\Integrations\Entities\CostlockerCompany;
 
@@ -26,7 +25,7 @@ class UpdateSettings
     {
         $company = $this->getUser->getCostlockerUser()->costlockerCompany;
         $company->settings = $settings;
-        $company->defaultBasecampUser = $this->findBasecampUser($settings['account']);
+        $company->defaultBasecampUser = $this->findBasecampUser($company, $settings['account']);
         $company->defaultCostlockerUser = $this->findCostlockerUser($company, $settings['costlockerUser']);
 
         $this->registerWebhook->__invoke($company);
@@ -34,15 +33,30 @@ class UpdateSettings
         $this->entityManager->flush();
     }
 
-    private function findBasecampUser($id)
+    private function findBasecampUser(CostlockerCompany $c, $id)
     {
-        return $this->entityManager
-            ->getRepository(BasecampUser::class)
-            ->find((int) $id);
+        if (!$id) {
+            return null;
+        }
+        $dql =<<<DQL
+            SELECT b
+            FROM Costlocker\Integrations\Entities\BasecampUser b
+            JOIN b.costlockerUser c
+            WHERE b.id = :id AND c.costlockerCompany = :company
+DQL;
+        $params = [
+            'id' => $id,
+            'company' => $c->id,
+        ];
+        $entities = $this->entityManager->createQuery($dql)->execute($params);
+        return reset($entities) ?: null;
     }
 
     private function findCostlockerUser(CostlockerCompany $c, $email)
     {
+        if (!$email) {
+            return null;
+        }
         return $this->entityManager
             ->getRepository(CostlockerUser::class)
             ->findOneBy(['costlockerCompany' => $c->id, 'email' => $email]);
