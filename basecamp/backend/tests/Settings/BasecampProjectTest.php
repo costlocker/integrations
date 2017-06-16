9f45f80d78cfe25bed571a57ac0d7ec4a473d2f2
Zdeněk Drahoš <drahoszdenek@gmail.com>
2017-06-16 11:31:02 +0200

    Basecamp - webhooks - commit forgotten file

---
+++ basecamp/backend/tests/Settings/BasecampProjectTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Costlocker\Integrations\Entities;
+
+class BasecampProjectTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDetectNewValue()
+    {
+        $project = new BasecampProject();
+        $project->updateSettings(['isBasecampWebhookEnabled' => false]);
+        assertThat($project->isNotChangedSetting('isBasecampWebhookEnabled'), is(false));
+    }
+
+    public function testDetectChangedValue()
+    {
+        $project = new BasecampProject();
+        $project->updateSettings(['isBasecampWebhookEnabled' => false]);
+        $project->updateSettings(['isBasecampWebhookEnabled' => true]);
+        assertThat($project->isNotChangedSetting('isBasecampWebhookEnabled'), is(false));
+    }
+
+    public function testIgnoreNoChangeInValue()
+    {
+        $project = new BasecampProject();
+        $project->updateSettings(['isBasecampWebhookEnabled' => false]);
+        $project->updateSettings(['isBasecampWebhookEnabled' => false]);
+        assertThat($project->isNotChangedSetting('isBasecampWebhookEnabled'), is(true));
+    }
+}
