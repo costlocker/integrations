0a8d46ef3beea61eaa209b5d34ed428613615032
Zdeněk Drahoš <drahoszdenek@gmail.com>
2017-07-26 13:29:30 +0200

    Fakturoid - reorganise invoice form

--- fakturoid/frontend/src/app/Invoice.js
+++ fakturoid/frontend/src/app/Invoice.js
@@ -5,28 +5,15 @@ import { PageWithSubnav, Page } from '../ui/App';
 import { isDevelopmentMode } from '../config';
 
 const InvoiceDetail = ({ costlocker }) => (
-  <table className="table">
-    <thead>
-      <tr>
-        <th style={{ width: '50%' }}>Bill ID / Description</th>
-        <th>Client</th>
-        <th>Project</th>
-        <th>Billing Date</th>
-        <th>Amount</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>{costlocker.billing.billing.description}</td>
-        <td>{costlocker.project.client.name}</td>
-        <td>
-          {costlocker.project.name} <span className="badge">{costlocker.project.project_id.id}</span>
-        </td>
-        <td>{costlocker.billing.billing.date}</td>
-        <td>{costlocker.billing.billing.total_amount}</td>
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <h1>
+      New Invoice
+      {costlocker.project.project_id.id ? <span> / {costlocker.project.project_id.id}</span> : null}
+    </h1>
+    <p>
+      <strong>{costlocker.project.name}</strong>, <span>{costlocker.project.client.name}</span>
+    </p>
+  </div>
 );
 
 const loadVat = (subjects, form) => {
@@ -57,10 +44,32 @@ const InvoiceEditor = ({ fakturoidSubjects, costlocker, form, lines, reloadSubje
   loadVat(fakturoidSubjects, form);
 
   return <form className="form" onSubmit={form.submit}>
+    <div>
+      <input
+        className="form-control" type="hidden" id="vat" min="0" max="100" step="1"
+        value={form.get('vat')} required
+      />
+    </div>
     <div className="form-group">
-      <label htmlFor="fakturoidSubject">Fakturoid customer</label>
+      <label htmlFor="type">Invoice type</label><br />
+      <div className="btn-group">
+        {[
+          { id: 'invoice', title: 'Invoice' },
+          { id: 'proforma.full', title: 'Proforma (full)' },
+          { id: 'proforma.partial', title: 'Proforma (partial)' },
+        ].map(type => (
+          <label key={type.id} className={form.get('type') === type.id ? 'btn btn-primary active' : 'btn btn-default'}>
+            <input
+              type="radio" name="type" value={type.id} className="hide"
+              checked={form.get('type') === type.id} onChange={form.set('type')} /> {type.title}
+          </label>
+        ))}
+      </div>
+    </div>
+    <div className="form-group">
+      <label htmlFor="fakturoidSubject">Fakturoid client</label>
       <div className="row">
-        <div className="col-sm-10">
+        <div className="col-sm-8">
           <select required
             className="form-control" name="fakturoidSubject" id="fakturoidSubject"
             value={form.get('subject')} onChange={form.set('subject')}
@@ -71,164 +80,153 @@ const InvoiceEditor = ({ fakturoidSubjects, costlocker, form, lines, reloadSubje
             ))}
           </select>
         </div>
-        <div className="col-sm-2">
-          <Link route='createSubject' title="Create a new customer" className="btn btn-default btn-block" />
+        <div className="col-sm-4">
+          <Link route='createSubject' title="Add a new Fakturoid client" className="btn btn-success btn-block" />
         </div>
       </div>
-      <p className="help-block text-right">
-        Are customers out of date? <Link action={reloadSubjects} title="Download customers from Fakturoid" className="text-danger" />
+      <p className="help-block">
+        Can't see a specific client? <Link
+          action={reloadSubjects}
+          title={<span><i className="fa fa-refresh" /> Try refreshing the list</span>}
+          className="btn btn-link"
+        />
       </p>
     </div>
-    <h3>Invoice lines</h3>
-    <div className="btn-toolbar">
-      <div className="btn-group">
-        <Link title="Add activities" action={lines.addActivities(costlocker.project.budget.peoplecosts)} className="btn btn-default" />
-        <Link title="Add people" action={lines.addPeople(costlocker.project.budget.peoplecosts)} className="btn btn-default" />
+    <div className="form-group">
+      <div className="row">
+        <div className="col-sm-10">
+          <div className="btn-toolbar">
+            <div className="btn-group">
+              <Link title="Add activities" action={lines.addActivities(costlocker.project.budget.peoplecosts)} className="btn btn-primary" />
+              <Link title="Add people" action={lines.addPeople(costlocker.project.budget.peoplecosts)} className="btn btn-primary" />
+            </div>
+            <div className="btn-group">
+              <Link title="Add expenses" action={lines.addExpenses(costlocker.project.budget.expenses)} className="btn btn-primary" />
+              <Link
+                title="Add discount" action={lines.addDiscount(costlocker.project.budget.discount)}
+                className={`btn btn-primary ${costlocker.project.budget.discount ? '' : 'disabled'}`} />
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-2">
+          <div className="btn-toolbar">
+            <div className="btn-group-justified">
+              <Link title="Reset lines" action={lines.removeAllLines()} className="btn btn-default" />
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="btn-group">
-        <Link title="Add expenses" action={lines.addExpenses(costlocker.project.budget.expenses)} className="btn btn-default" />
-        <Link
-          title="Add discount" action={lines.addDiscount(costlocker.project.budget.discount)}
-          className={`btn btn-default ${costlocker.project.budget.discount ? '' : 'disabled'}`} />
-      </div>
-      <div className="btn-group">
-        <Link title="Add empty line" action={lines.addEmptyLine()} className="btn btn-default" />
-        <Link title="Reset lines" action={lines.removeAllLines()} className="btn btn-default" />
-      </div>
-    </div>
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th width="100">Quantity</th>
-          <th width="100">Unit</th>
-          <th>Unit Amount</th>
-          <th>Total Amount</th>
-          <th width="10"></th>
-        </tr>
-      </thead>
-      <tbody>
-        {lines.map((line) => {
-          const isLineIgnored = line.get('quantity') <= 0;
-          return <tr
-            key={line.get('id')}
-            className={isLineIgnored ? 'bg-danger' : null}
-            title={isLineIgnored ? 'Line will be ignored and not imported to Fakturoid' : null}
-          >
-            <td>
-              <input
-                className="form-control" type="text" required
-                value={line.get('name')} onChange={lines.updateFieldInLine('name', line)}
-              />
-            </td>
-            <td>
-              <input
-                className="form-control" type="number" step="any" required
-                value={line.get('quantity')} onChange={lines.updateFieldInLine('quantity', line)}
-              />
-            </td>
-            <td>
-              <input
-                className="form-control" type="text"
-                value={line.get('unit')} onChange={lines.updateFieldInLine('unit', line)}
-              />
-            </td>
-            <td>
-              <input
-                className="form-control" type="number" step="any" required size="10"
-                value={roundNumber(line.get('unit_amount'))} onChange={lines.updateFieldInLine('unit_amount', line)}
-              />
-            </td>
-            <td>
-              <input
-                className="form-control" type="number" step="any" required
-                disabled value={roundNumber(line.get('total_amount'))}
-              />
-            </td>
-            <td>
-              {lines.hasMultipleLines() ? (
-                <Link
-                  title={<span className="fa fa-trash"></span>} className="btn btn-link text-danger"
-                  action={lines.removeLine(line)}
+      <table className="table">
+        <thead>
+          <tr>
+            <th width="100">Quantity</th>
+            <th width="100">Unit</th>
+            <th>Name</th>
+            <th>Price per unit</th>
+            <th>Total price</th>
+            <th width="10"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {lines.map((line) => {
+            const isLineIgnored = line.get('quantity') <= 0;
+            return <tr
+              key={line.get('id')}
+              className={isLineIgnored ? 'bg-danger' : null}
+              title={isLineIgnored ? 'Line will be ignored and not imported to Fakturoid' : null}
+            >
+              <td>
+                <input
+                  className="form-control" type="number" step="any" required
+                  value={line.get('quantity')} onChange={lines.updateFieldInLine('quantity', line)}
                 />
-              ) : <span className="btn btn-link disabled fa fa-trash"></span>}
-            </td>
-          </tr>;
-        })}
-      </tbody>
-      {form.get('hasVat') ? (
-        <tfoot>
-          <tr>
-            <th colSpan="4" className="text-right">Total amount (without VAT)</th>
-            <th colSpan="2"><Number value={linesAmount} isElement /></th>
-          </tr>
-          <tr>
-            <th colSpan="4" className="text-right">VAT</th>
-            <th colSpan="2"><Number value={form.get('vat')} isElement />%</th>
-          </tr>
-          <tr>
-            <th colSpan="4" className="text-right">Total amount (with VAT)</th>
-            <th colSpan="2"><Number value={linesAmount + linesAmount * (form.get('vat')) / 100} isElement /></th>
-          </tr>
-        </tfoot>
-      ) : (
+              </td>
+              <td>
+                <input
+                  className="form-control" type="text"
+                  value={line.get('unit')} onChange={lines.updateFieldInLine('unit', line)}
+                />
+              </td>
+              <td>
+                <input
+                  className="form-control" type="text" required
+                  value={line.get('name')} onChange={lines.updateFieldInLine('name', line)}
+                />
+              </td>
+              <td>
+                <input
+                  className="form-control" type="number" step="any" required size="10"
+                  value={roundNumber(line.get('unit_amount'))} onChange={lines.updateFieldInLine('unit_amount', line)}
+                />
+              </td>
+              <td>
+                <input
+                  className="form-control" type="number" step="any" required
+                  disabled value={roundNumber(line.get('total_amount'))}
+                />
+              </td>
+              <td>
+                {lines.hasMultipleLines() ? (
+                  <Link
+                    title={<span className="fa fa-trash"></span>} className="btn btn-link text-danger"
+                    action={lines.removeLine(line)}
+                  />
+                ) : <span className="btn btn-link disabled fa fa-trash"></span>}
+              </td>
+            </tr>;
+          })}
+        </tbody>
+        {form.get('hasVat') ? (
           <tfoot>
             <tr>
-              <th colSpan="4" className="text-right">Total amount</th>
+              <td colSpan="6">
+                <Link title="Add new empty line" action={lines.addEmptyLine()} className="btn btn-success" />
+              </td>
+            </tr>
+            <tr>
+              <th colSpan="4" className="text-right">Total amount (without VAT)</th>
               <th colSpan="2"><Number value={linesAmount} isElement /></th>
             </tr>
+            <tr>
+              <th colSpan="4" className="text-right">VAT</th>
+              <th colSpan="2"><Number value={form.get('vat')} isElement />%</th>
+            </tr>
+            <tr>
+              <th colSpan="4" className="text-right">Total amount (with VAT)</th>
+              <th colSpan="2"><Number value={linesAmount + linesAmount * (form.get('vat')) / 100} isElement /></th>
+            </tr>
           </tfoot>
-        )}
-    </table>
-    <div className="row">
-      <div className="col-sm-4">
-        <div className="form-group">
-          <label htmlFor="type">Invoice type</label><br />
-          {[
-            { id: 'invoice', title: 'Invoice' },
-            { id: 'proforma.full', title: 'Proforma (full)' },
-            { id: 'proforma.partial', title: 'Proforma (partial)' },
-          ].map(type => (
-            <label className="radio-inline" key={type.id}>
-              <input
-                type="radio" name="type" value={type.id}
-                checked={form.get('type') === type.id} onChange={form.set('type')}
-              /> {type.title}
-            </label>
-          ))}
-        </div>
-        <div className="form-group">
-          <label htmlFor="vat">VAT</label><br />
-          {form.get('hasVat') ? (
-            <input
-              className="form-control" type="number" id="vat" min="0" max="100" step="1"
-              value={form.get('vat')} onChange={form.set('vat')} required
-            />
-          ) : (
-              <p className="text-muted">Customer doesn't have VAT number</p>
-            )}
-        </div>
-      </div>
-      <div className="col-sm-8">
-        <div className="form-group">
-          <label htmlFor="note">Note</label>
-          <textarea
-            className="form-control" name="note" id="note" rows="4"
-            placeholder="Add private note to invoice..."
-            value={form.get('note')} onChange={form.set('note')}
-          >
-          </textarea>
-        </div>
-      </div>
+        ) : (
+            <tfoot>
+              <tr>
+                <th colSpan="4" className="text-right">Total amount</th>
+                <th colSpan="2"><Number value={linesAmount} isElement /></th>
+              </tr>
+            </tfoot>
+          )}
+      </table>
     </div>
     {Math.abs(billedAmount - linesAmount) <= 0.1 ? (
-      <button type="submit" className="btn btn-primary btn-block">Create invoice</button>
+      <div className="row">
+        <div className="col-sm-4 col-sm-offset-8">
+          <button type="submit" className="btn btn-primary btn-block">Create Invoice</button>
+        </div>
+      </div>
     ) : (
-        <Errors
-          title={`Billed amount '${roundNumber(billedAmount)}' in Costlocker is different than total amount in invoice lines '${roundNumber(linesAmount)}'`}
-          error="Update quantity or unit amount in lines, so that amount is same"
-        />
+      <Errors
+        title={`Billed amount '${roundNumber(billedAmount)}' in Costlocker is different than total amount in invoice lines '${roundNumber(linesAmount)}'`}
+        error="Update quantity or unit amount in lines, so that amount is same"
+      />
       )}
+    <div className="form-group">
+      <label htmlFor="note">Note</label>
+      <textarea
+        className="form-control" name="note" id="note" rows="4"
+        placeholder="Add private note to invoice..."
+        value={form.get('note')} onChange={form.set('note')}
+      >
+      </textarea>
+    </div>
   </form>;
 }
 
@@ -240,10 +238,11 @@ export default function Invoice(props) {
         {
           id: 'invoice',
           name: 'New invoice',
-          content: () => <div>
-            <h3>Costlocker billing</h3>
-            <InvoiceDetail costlocker={props.invoice.costlocker} />
-            {content}
+          content: () => <div className="row">
+            <div className="col-sm-10 col-sm-offset-1">
+              <InvoiceDetail costlocker={props.invoice.costlocker} />
+              {content}
+            </div>
           </div>,
         },
         {
