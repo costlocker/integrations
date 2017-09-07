a8a8437147031cacf29c66984c70fa24dc37338c
Zdeněk Drahoš <drahoszdenek@gmail.com>
2017-09-07 09:16:45 +0200

    Basecamp - responsive navbar - spike navbar with jquery+bootstrap

--- basecamp/frontend/src/ui/App.js
+++ basecamp/frontend/src/ui/App.js
@@ -26,6 +26,7 @@ const BasecampUser = ({ user }) => {
 
 const AnonymousUser = ({ isRouteActive }) => (
   <div>
+    <span className="hide">Login</span>
     <Logo app="costlocker" color={isRouteActive('login') ? 'blue' : 'white'} />
     <User name="Login" company="Costlocker" />
   </div>
@@ -33,6 +34,7 @@ const AnonymousUser = ({ isRouteActive }) => (
 
 const Users = ({ auth, isRouteActive }) => {
   return <div>
+    <span className="hide">Accounts</span>
     <Logo app="costlocker" color={isRouteActive('accounts') ? 'blue' : 'white'} />
     <CostlockerUser user={auth.get('costlocker')} />
     <Logo app="basecamp" />
@@ -56,23 +58,44 @@ export default function Layout({ auth, isRouteActive }) {
   return (
     <div>
       <nav className="navbar navbar-default">
-        <div className="container">
-          <div>
-            {auth.get('costlocker') ? <Navigation isRouteActive={isRouteActive} routes={[
-              { route: 'projects', title: 'Projects' },
-              { route: 'sync', title: 'Add project', params: { clProject: null } },
-              { route: 'events', title: 'Events', params: { clProject: null } },
-              { route: 'settings', title: 'Settings' },
-            ]} /> : ''}
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button
+              type="button" className="navbar-toggle collapsed" data-toggle="collapse"
+              data-target="#navbar-addon" aria-expanded="false"
+            >
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
           </div>
-          <div>
-            <Navigation className="navbar-right text-right" isRouteActive={isRouteActive} routes={[
-              { route: 'accounts', activeRoute: 'login', className: 'users', title: auth.get('costlocker')
-                ? <Users auth={auth} isRouteActive={isRouteActive} />
-                : <AnonymousUser isRouteActive={isRouteActive} />
-              },
-              { route: 'help', title: <span className="fa fa-question-circle"></span> },
-            ]} />
+          <div className="navbar-collapse collapse" id="navbar-addon">
+            <nav>
+              <div className="container">
+                <div>
+                  {auth.get('costlocker') ? <Navigation isRouteActive={isRouteActive} routes={[
+                    { route: 'projects', title: 'Projects' },
+                    { route: 'sync', title: 'Add project', params: { clProject: null } },
+                    { route: 'events', title: 'Events', params: { clProject: null } },
+                    { route: 'settings', title: 'Settings' },
+                  ]} /> : ''}
+                </div>
+                <div>
+                  <Navigation className="navbar-right text-right" isRouteActive={isRouteActive} routes={[
+                    { route: 'accounts', activeRoute: 'login', className: 'users', title: auth.get('costlocker')
+                      ? <Users auth={auth} isRouteActive={isRouteActive} />
+                      : <AnonymousUser isRouteActive={isRouteActive} />
+                    },
+                    { route: 'help', className: 'help', title: <div>
+                        <span className="fa fa-question-circle"></span>
+                        <span className="hide">Help</span>
+                      </div>
+                    },
+                  ]} />
+                </div>
+              </div>
+            </nav>
           </div>
         </div>
       </nav>
